404c66cf1dbd9ee54b3e8b9946dcc195dafd4f40
Nicola37 <nicolafrachesen@bellsouth.net>
2014-04-04 12:42:07 -0400

    Test commit. Basics added to TranscendentalNumber.h

--- TranscendentalNumber.h
+++ TranscendentalNumber.h
@@ -13,7 +13,15 @@
 #ifndef TRANSCENDENTALNUMBER_H
 #define	TRANSCENDENTALNUMBER_H
 
+class TranscendentalNumber :: public Number{
+public:
+	TranscendentalNumber(enum value, int coefficient);
+	simplify();
 
+private:
+	enum value;
+	int coefficient;
+}
 
 #endif	/* TRANSCENDENTALNUMBER_H */
 

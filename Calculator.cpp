6ed6a37cd9f28eb49e055bf78572a33226f5f9d2
Nicola37 <nicolafrachesen@bellsouth.net>
2014-04-08 19:43:18 -0400

    Basics added to Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:        TranscendentalNumber.cpp
+ * 
+ * Authors:     Brent Lewis
+ *              Jacob Jenkins
+ *              Shreya Nirmalan
+ *              Nicola Frachesen
+ *              Elaine Del Rosario
+ * 
+ * 
+ */
+
+#include "Calculator.h"
+
+using namespace std;
+
+Calculator::Calculator() {
+}
+
+void calculate(){
+  //stuff will go here
+}

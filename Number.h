a8984b617c8d11722e10996174fdbedac607f651
Jacob Jenkins <jacob@jacobejenkins.com>
2014-04-10 12:14:58 -0400

    Commented out operator overloads in Number.

--- Number.h
+++ Number.h
@@ -25,10 +25,10 @@ class Number
 public:
     virtual void simplify() = 0;
     virtual void display() = 0;
-	virtual Number& operator+(const Number& number) = 0;
-	virtual Number& operator-(const Number& number) = 0;
-	virtual Number& operator*(const Number& number) = 0;
-	virtual Number& operator/(const Number& number) = 0;
+	// virtual Number& operator+(const Number& number) = 0;
+	// virtual Number& operator-(const Number& number) = 0;
+	// virtual Number& operator*(const Number& number) = 0;
+	// virtual Number& operator/(const Number& number) = 0;
 };
 
 #endif	/* NUMBER_H */

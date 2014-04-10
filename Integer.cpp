4ff58a2bece6cd2c8defa4c6fee8d4de9fe7bff8
Elaine Del Rosario <e.delrosario94@gmail.com>
2014-04-10 10:14:31 -0400

    Added operation overloading methods and changed int to long

--- Integer.cpp
+++ Integer.cpp
@@ -10,19 +10,20 @@
  *
  */
 
+#include <stdexcept>
 #include "Integer.h"
 
 // Constructor
-Integer::Integer(int value) {
+Integer::Integer(long value) {
     this->value = value;
 }
 
 // Get-Set methods
-int Integer::getValue() {
+long Integer::getValue() {
     return value;
 }
 
-void Integer::setValue(int value) {
+void Integer::setValue(long value) {
     this->value = value;
 }
 
@@ -30,3 +31,34 @@ void Integer::setValue(int value) {
 void Integer::simplify() {
     // Exists to satisfy Number Parent class.
 }
+
+Integer Integer:: operator+(const Integer &integer) {
+    Integer temp;
+    temp.value = value + integer.value;
+    return temp;
+}
+
+Integer Integer:: operator-(const Integer &integer) {
+    Integer temp;
+    temp.value = value - integer.value;
+    return temp;
+}
+
+Integer Integer:: operator*(const Integer &integer) {
+    Integer temp;
+    temp.value = value * integer.value;
+    return temp;
+}
+
+Integer Integer:: operator/(const Integer &integer) {
+    try {
+        Integer temp;
+        if (integer.value == 0) {
+            throw invalid_argument("Cannot divide by 0!");
+        }
+        catch (invalid_argument & ex) {
+            cerr << ex.what() << endl;
+        }
+        temp.value = value / integer.value;
+        return temp;
+    }

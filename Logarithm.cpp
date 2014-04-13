dbcaed3ca136b9f0a413b21c95bd17f461f73368
Elaine Del Rosario <e.delrosario94@gmail.com>
2014-04-12 23:00:29 -0400

    Fixed errors

--- Logarithm.cpp
+++ Logarithm.cpp
@@ -6,7 +6,7 @@ using namespace std;
  * TODO: Write a little bit about what this class does in relation to Expression
  */
 
-Logarithm::Logarithm(Number coefficient, vector<Number*> LogValues, Number base) {
+Logarithm::Logarithm(Number* coefficient, vector<Number*> LogValues, Number* base) {
 	this->values["coefficient"] = coefficient;
 	this->LogValues = LogValues;
 	this->values["base"] = base;
@@ -14,7 +14,7 @@ Logarithm::Logarithm(Number coefficient, vector<Number*> LogValues, Number base)
 
 Logarithm::~Logarithm() {
     delete values["coefficient"];
-    delete values;
+    delete LogValues;
     delete values["base"];
 }
 
@@ -27,7 +27,7 @@ void Logarithm::setValues(string key, Number* val) {
     values[key] = val;
 }
 vector<Number*> Logarithm::getLogValues() {
-    return values;
+    return LogValues;
 }
 void Logarithm::setLogValues(vector<Number*> LogValues) {
     this->LogValues = LogValues;
@@ -37,15 +37,15 @@ void Logarithm::setLogValues(vector<Number*> LogValues) {
 void Logarithm::simplify() {
     vector<int> primes;
     if (typeid(LogValues[0]) == typeid(Integer)){
-    	vector<int> primes = findPrimeFactors(LogValues[0].getValue(), 2, primes);
+    	primes = findPrimeFactors(LogValues[0]->getValue(), 2, primes);
     	splitLog(primes);
     }
     if (typeid(LogValues[0]) == typeid(RationalNumber)){
     	//Splits a log of a RationalNumber into a log of its numerator and denominator
     	//separated by a minus.
-    	RationalNumber temp = LogValues[0];
-        LogValues[0] = temp.getValues()["numerator"];
-        LogValues.push_back(temp.getValues()["denominator"]);
+    	Number* temp = LogValues[0];
+        LogValues[0] = temp->getValues()["numerator"];
+        LogValues.push_back(temp->getValues()["denominator"]);
         smoothOperator = '-';
     }
 }
@@ -60,24 +60,24 @@ double Logarithm::toDouble(){
 	//Uses log() from cmath which gives the natural logarithm.
 	//Either subtracts or adds, depending on smoothOperator.
 	double tempSum;
-	for (i = 0; i >= LogValues.size(), i++){
-		if (smoothOperator = '+'){
-			tempSum += values["coefficient"]->toDouble()*(log(LogValues[i]->toDouble())/log(values["base"]->toDouble));
+	for (int i = 0; i >= LogValues.size(); i++){
+		if (smoothOperator == '+'){
+			tempSum += values["coefficient"]->toDouble()*(log(LogValues[i]->toDouble())/log(values["base"]->toDouble()));
 		}
 		else {
-			tempSum -= values["coefficient"]->toDouble()*(log(LogValues[i]->toDouble())/log(values["base"]->toDouble));
+			tempSum -= values["coefficient"]->toDouble()*(log(LogValues[i]->toDouble())/log(values["base"]->toDouble()));
 		}
 	}
 	return tempSum;
 }
 string Logarithm::toString(){
 	stringstream valueStream;
-	valueStream << values["coefficient"]->toString()
-	for (i = 0; i >= LogValues.size(), i++){
-		if (i = LogValues.size()-1){
+	valueStream << values["coefficient"]->toString();
+	for (int i = 0; i >= LogValues.size(); i++){
+		if (i == LogValues.size()-1){
 			valueStream << "log_" << values["base"]->toString() << ":" << LogValues[i]->toString();
 		}
-		else if (smoothOperator = '+'){
+		else if (smoothOperator == '+'){
 			valueStream << "log_" << values["base"]->toString() << ":" << LogValues[i]->toString() << "+";
 		}
 		else{

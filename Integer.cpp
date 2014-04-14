cc6e834ac6325d812a08d3216992c7ae2380efe1
Nicola37 <nicolafrachesen@bellsouth.net>
2014-04-14 17:38:45 -0400

    exponentiate changed

--- Integer.cpp
+++ Integer.cpp
@@ -128,7 +128,7 @@ Number* Integer::exponentiate(Number* val)
 {
 	if (typeid(*val) == typeid(Integer)) {
 		long valLong;
-		for (long i = 0; i < val->getValue(); i++){
+		for (long i = 1; i < val->getValue(); i+=1){
 			valLong += value*value;
 		}
 		//double valLong = pow(toDouble(), val->toDouble());

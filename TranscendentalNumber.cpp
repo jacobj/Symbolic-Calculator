26bc4f3c72b46097ea3eb129b331ec93b045aff6
Nicola37 <nicolafrachesen@bellsouth.net>
2014-04-22 09:32:44 -0400

    Exponentiate now handles another case.

--- TranscendentalNumber.cpp
+++ TranscendentalNumber.cpp
@@ -198,6 +198,9 @@ Number* TranscendentalNumber::exponentiate(Number* val){
 	if (typeid(*val) == typeid(Integer) && val->getValue() == 0){
 		return new Integer("1");
 	}
+	else if (typeid(*val) == typeid(Integer) && val->getValue() == 1){
+			return new TranscendentalNumber(value);
+	}
 	else if(!values["coefficient"]->toString().compare("1")){
 		valStream << value << "^" << val->toString();
 		string str = valStream.str();
